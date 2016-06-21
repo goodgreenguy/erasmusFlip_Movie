3d5285798862f3ca85f789bc1519f881982e0633
Ivan Ignac <iignac@gmail.com>
2016-06-21 13:36:40 +0200

    Added boxes

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php include 'header.php'; ?>
-
 <body>
+<div class="container">
 
+	<div class="row">
+	</div>
+	<div class="row">
+		<div id="char_box" class="col-md-6 floating">
+			<img src="img/box_cover.png" class="box" alt="Character">
+			<img src="img/box_body.png" class="box box_align" alt="Character" >
+		</div>
+
+		<div id="plot_box" class="col-md-6 floating">
+			<img src="img/box_cover.png" class="box" alt="Plot">
+			<img src="img/box_body.png" class="box box_align" alt="Plot" >
+		</div>
+	</div>
+	
+	<div class="row">
+		<div id="setting_box" class="col-md-6 floating">
+			<img src="img/box_cover.png" class="box" alt="Setting">
+			<img src="img/box_body.png" class="box box_align" alt="Setting" >
+		</div>
+		<div id="ends_box" class="col-md-6 floating">
+			<img src="img/box_cover.png" class="box" alt="Endings">
+			<img src="img/box_body.png" class="box box_align" alt="Endings" >
+		</div>
+	</div>
+	<div class="row mystery">
+		<div id="mystery_box" class="col-md-6 floating">
+			<img src="img/box_cover.png" class="box" alt="Mystery">
+			<img src="img/box_body.png" class="box box_align" alt="Mystery" >
+		</div>
+	</div>
+</div>
 
 <script type="text/javascript" >
 
